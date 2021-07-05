18c174fa688668211d6ac7c11940f5d625447e33
Vansh Sethi <44345210+Vansh-Sethi@users.noreply.github.com>
2021-07-05 19:15:33 -0400

    Getting Started Assignment

--- assignments/getting-started.pde
+++
@@ -1,7 +0,0 @@
-void setup() {
-    size(500,500);
-}
-
-void draw() {
-    circle(250.0, 250.0, 50);
-}
--- out/source/ICS3U.java
+++ out/source/ICS3U.java
@@ -19,9 +19,30 @@ public void setup() {
 }
 
 public void draw() {
-    circle(250.0f, 250.0f, 50);
+    strokeWeight(5);
+    fill(255,0,0);
+    rect(85,5,325,500, 200,200,0,0);
+    
+    strokeWeight(5);
+    fill(255,0,0);
+    rect(85,505,130,100, 0,0,200,200);
+
+    strokeWeight(5);
+    fill(255,0,0);
+    rect(282,505,130,100, 0,0,200,200);
+
+    strokeWeight(5);
+    fill(255,0,0);
+    rect(3,200,78,300, 200,0,0,200);
+
+    strokeWeight(5);
+    fill(120, 255, 237);
+    rect(175,75,300,180, 500,500,500,500);
+
+
+
 }
-  public void settings() {  size(500,500); }
+  public void settings() {  size(477, 640); }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "ICS3U" };
     if (passedArgs != null) {
