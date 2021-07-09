c19410448cddab5f7528d99c1e7922acf8105537
Vansh Sethi <44345210+Vansh-Sethi@users.noreply.github.com>
2021-07-08 21:49:54 -0400

    formative for iterators

--- out/source/ICS3U.java
+++ out/source/ICS3U.java
@@ -15,96 +15,32 @@ import java.io.IOException;
 public class ICS3U extends PApplet {
 
 /* 
-ICS3U - Assigment #1
-Title: Coincentric Circles
+ICS3U - Assigment #2
+Title: Quadratic Lines
 Author: Vansh Sethi
 */
 
 /*
-The major idea being implemented: Check if the distance to the center of the circle is smaller than the radius, meaning
-that it is inside the circle. We do this 3 times for all the radii.
+The major idea being implemented: Basically, the entire screen can be broken up into 4 quadrants of the same pattern.
+To achieve the pattern, we must draw lines that create a right angle to the corner of the quadrant. This can
+be achieved with the use of a for loop, that draws lines from one corner of the quadrant to the adjacent side
+until it reaches the other corner of the quadrant. Then, we simplyrepeat the final pattern to another quadrant. In the code, this is acheived with the xFactor and yFactor respectively.
 
-The alternative 'solution' is treating the circle as a rectangle, however this would discount many cases.
+The alternative solution is hardcoding each case, however this would make the code messy and unmaintable. Therefore, this 
+approach wask taken.
 */
 
 public void setup() {
-    // Define canvas size
-    
+  
+
+  for (int i = 50; i <= 450; i+=50) {
+    for (int j = 50; j <= 450; j+=50) {
+      fill(0,0,0);
+      circle(i, j, 5);
+    }
+  }
 }
-
-public void draw() {
-    // Define background color
-    background(214, 214, 214);
-
-    // Small cirlce (check if radius is below 100)
-    if (dist(mouseX, mouseY, 300,300) <= 100) {
-        clear(); 
-        background(214, 214, 214);
-
-        fill(255,255,255);
-        circle(300, 300, 600);
-        circle(300, 300, 400);
-
-        // Change the color of the full circle 
-        fill(242, 255, 3);
-        circle(300, 300, 200);
-
-        // Change the color of the full circle 
-        line(mouseX, mouseY, mouseX, mouseY+10);
-        line(mouseX, mouseY, mouseX, mouseY-10);
-        line(mouseX, mouseY, mouseX+10, mouseY);
-        line(mouseX, mouseY, mouseX-10, mouseY);
-    }
-
-    // Medium Circle (check if radius is below 200)
-    else if (dist(mouseX, mouseY, 300,300) <= 200) {
-        clear();
-        background(214, 214, 214);
-
-        fill(255,255,255);
-        circle(300, 300, 600);
-
-        fill(34, 255, 5);
-        circle(300, 300, 400);
-
-        fill(255,255,255);
-        circle(300, 300, 200);
-
-        fill(255,255,255);
-        rect(mouseX, mouseY, 20, 20);
-    }
-
-    // Large Circle (check if radius is below 300)
-    else if (dist(mouseX, mouseY, 300,300) <= 300) {
-        clear();
-        background(214, 214, 214);
-
-        fill(255,0,0);
-        circle(300, 300, 600);
-
-        fill(255,255,255);
-        circle(300, 300, 400);
-
-        fill(255,255,255);
-        circle(300, 300, 200);
-
-        fill(255,255,255);
-        circle(mouseX, mouseY, 20);
-    }
-
-    // Last Case (Cursor not in any of the circles)
-    else {
-        clear();
-        background(214, 214, 214);
-        
-        fill(255,255,255);
-        circle(300, 300, 600);
-        circle(300, 300, 400);
-        circle(300, 300, 200);
-    }
-
-}
-  public void settings() {  size(600, 600); }
+  public void settings() {  size(500, 500); }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "ICS3U" };
     if (passedArgs != null) {
