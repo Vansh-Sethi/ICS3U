f279add700f557f5f712c1e0347b4360e6a58a77
Vansh Sethi <44345210+Vansh-Sethi@users.noreply.github.com>
2021-07-19 19:55:20 -0400

    Assignment 5 mostly done

--- ICS3U.pde
+++ ICS3U.pde
@@ -1,54 +1,132 @@
 /* 
-ICS3U - Formative Classes
-Title: SquareBox Classes
+ICS3U - Assignment 5
+Title: Parallax Triangles
 Author: Vansh Sethi
 */
 
-// Create instances of SquareBoxs
-Squarebox Square1 = new Squarebox(200, 200, 50);
-Squarebox Square2 = new Squarebox(400, 400, 25);
-Squarebox Square3 = new Squarebox(50, 50, 100);
+// Create instances of Triangles
+Triangle[] LargeTriangles = new Triangle[25];
+Triangle[] MediumTriangles = new Triangle[20];
+Triangle[] SmallTriangles = new Triangle[15];
+// int x = 0;
+
 
 
 void setup() {
   // Set up the screen
  size(512, 512);
+//  pushMatrix();
+
+
+  // Set up the triangles
+  for (int i = 0; i < LargeTriangles.length; i++) {
+    LargeTriangles[i] = new Triangle(int(random(0, width)), int(random(0, height)), random(0.5, 1.5), "foreground", 2.2);
+  }
+
+  for (int i = 0; i < MediumTriangles.length; i++) {
+    MediumTriangles[i] = new Triangle(int(random(0, width)), int(random(0, height)), random(0.5, 1.5), "midground", 1.3);
+  }
+
+  for (int i = 0; i < SmallTriangles.length; i++) {
+    SmallTriangles[i] = new Triangle(int(random(0, width)), int(random(0, height)), random(0.5, 1.5), "background", 1.0);
+  }
+
+
+
 }
 
-// Draw and move the square boxes
+// Draw and move the triangles
 void draw() {
   clear();
-  Square1.display();
-  Square1.move(10);
-  Square2.display();
-  Square2.move(5);
-  Square3.display();
-  Square3.move(15);
+  background(0, 42, 47);
+  for (int i = 0; i < LargeTriangles.length; i++) {
+    LargeTriangles[i].display();
+  }
+
+  for (int i = 0; i < MediumTriangles.length; i++) {
+    MediumTriangles[i].display();
+  }
+
+  for (int i = 0; i < SmallTriangles.length; i++) {
+    SmallTriangles[i].display();
+  }
+  // int size = 35;
+  // triangle(200, 200, 200+(size/tan(PI/3)), 200-size, 200+2*(size/tan(PI/3)), 200);
+
+
+  // translate(220, 186);
+  // rotate(radians(x));
+  // x+=1;
+  // triangle(0, 0, (size/tan(PI/3)), size, 2*(size/tan(PI/3)), 0);
+
+
 }
 
-// Create the object Squarebox
-class Squarebox {
+// Create the object Triangle
+class Triangle {
 // Data 
-  int sideLength;
   int x;
   int y;
+  float rotateSpeed;
+  String type; 
+  int size; 
+  float move;
+  float speed;
+  color triColor;
 
 // Constructor (initalize variables)
-  Squarebox (int tempX, int tempY, int tempSideLength) {
+  Triangle (int tempX, int tempY, float tempRotateSpeed, String tempType, float tempSpeed) {
     x = tempX;
     y = tempY;
-    sideLength = tempSideLength;
-  }
-// Method that displays the square box
-  void display() {
-    rect(x, y, sideLength, sideLength);
-  }
-
-// Method that moves the square box
-  void move(int distance) { 
-    x += distance;
-    if (x - sideLength > 512) {
-      x = 0;
+    move = random(0, 100);
+    rotateSpeed = tempRotateSpeed;
+    type = tempType;
+    if (type == "foreground") {
+      size = 35;
+    }
+    else if (type == "midground") {
+      size = 25;
+    }
+    else {
+      size = 15;
+    }
+    speed = tempSpeed;
+    int a = int(random(0, 4));
+    if (a == 1) {
+      triColor = color(231, 223, 175);
+    }
+    if (a == 2) {
+      triColor = color(171, 163, 125);
+    }
+    if (a == 3) {
+      triColor = color(0, 93, 132);
+    }
+    else {
+      triColor = color(255, 255, 255);
     }
   }
+
+// Method that displays the triangle
+  void display() {
+    translate(x,y);
+    rotate(radians(move));
+    move+=rotateSpeed;
+    noStroke();
+    fill(triColor);
+    triangle(0, 0, (size/tan(PI/3)), size, 2*(size/tan(PI/3)), 0);
+    moveDown();
+    resetMatrix();
+
+
+  }
+
+  void moveDown() {
+    y+=speed;
+ 
+    
+    if (y-size > height) { 
+      y = 0;
+    }
+  }
+
 }
--- out/source/ICS3U.java
+++ out/source/ICS3U.java
@@ -15,58 +15,136 @@ import java.io.IOException;
 public class ICS3U extends PApplet {
 
 /* 
-ICS3U - Formative Classes
-Title: SquareBox Classes
+ICS3U - Assignment 5
+Title: Parallax Triangles
 Author: Vansh Sethi
 */
 
-// Create instances of SquareBoxs
-Squarebox Square1 = new Squarebox(200, 200, 50);
-Squarebox Square2 = new Squarebox(400, 400, 25);
-Squarebox Square3 = new Squarebox(50, 50, 100);
+// Create instances of Triangles
+Triangle[] LargeTriangles = new Triangle[25];
+Triangle[] MediumTriangles = new Triangle[20];
+Triangle[] SmallTriangles = new Triangle[15];
+// int x = 0;
+
 
 
 public void setup() {
   // Set up the screen
  
+//  pushMatrix();
+
+
+  // Set up the triangles
+  for (int i = 0; i < LargeTriangles.length; i++) {
+    LargeTriangles[i] = new Triangle(PApplet.parseInt(random(0, width)), PApplet.parseInt(random(0, height)), random(0.5f, 1.5f), "foreground", 2.2f);
+  }
+
+  for (int i = 0; i < MediumTriangles.length; i++) {
+    MediumTriangles[i] = new Triangle(PApplet.parseInt(random(0, width)), PApplet.parseInt(random(0, height)), random(0.5f, 1.5f), "midground", 1.3f);
+  }
+
+  for (int i = 0; i < SmallTriangles.length; i++) {
+    SmallTriangles[i] = new Triangle(PApplet.parseInt(random(0, width)), PApplet.parseInt(random(0, height)), random(0.5f, 1.5f), "background", 1.0f);
+  }
+
+
+
 }
 
-// Draw and move the square boxes
+// Draw and move the triangles
 public void draw() {
   clear();
-  Square1.display();
-  Square1.move(10);
-  Square2.display();
-  Square2.move(5);
-  Square3.display();
-  Square3.move(15);
+  background(0, 42, 47);
+  for (int i = 0; i < LargeTriangles.length; i++) {
+    LargeTriangles[i].display();
+  }
+
+  for (int i = 0; i < MediumTriangles.length; i++) {
+    MediumTriangles[i].display();
+  }
+
+  for (int i = 0; i < SmallTriangles.length; i++) {
+    SmallTriangles[i].display();
+  }
+  // int size = 35;
+  // triangle(200, 200, 200+(size/tan(PI/3)), 200-size, 200+2*(size/tan(PI/3)), 200);
+
+
+  // translate(220, 186);
+  // rotate(radians(x));
+  // x+=1;
+  // triangle(0, 0, (size/tan(PI/3)), size, 2*(size/tan(PI/3)), 0);
+
+
 }
 
-// Create the object Squarebox
-class Squarebox {
+// Create the object Triangle
+class Triangle {
 // Data 
-  int sideLength;
   int x;
   int y;
+  float rotateSpeed;
+  String type; 
+  int size; 
+  float move;
+  float speed;
+  int triColor;
 
 // Constructor (initalize variables)
-  Squarebox (int tempX, int tempY, int tempSideLength) {
+  Triangle (int tempX, int tempY, float tempRotateSpeed, String tempType, float tempSpeed) {
     x = tempX;
     y = tempY;
-    sideLength = tempSideLength;
-  }
-// Method that displays the square box
-  public void display() {
-    rect(x, y, sideLength, sideLength);
-  }
-
-// Method that moves the square box
-  public void move(int distance) { 
-    x += distance;
-    if (x - sideLength > 512) {
-      x = 0;
+    move = random(0, 100);
+    rotateSpeed = tempRotateSpeed;
+    type = tempType;
+    if (type == "foreground") {
+      size = 35;
+    }
+    else if (type == "midground") {
+      size = 25;
+    }
+    else {
+      size = 15;
+    }
+    speed = tempSpeed;
+    int a = PApplet.parseInt(random(0, 4));
+    if (a == 1) {
+      triColor = color(231, 223, 175);
+    }
+    if (a == 2) {
+      triColor = color(171, 163, 125);
+    }
+    if (a == 3) {
+      triColor = color(0, 93, 132);
+    }
+    else {
+      triColor = color(255, 255, 255);
     }
   }
+
+// Method that displays the triangle
+  public void display() {
+    translate(x,y);
+    rotate(radians(move));
+    move+=rotateSpeed;
+    noStroke();
+    fill(triColor);
+    triangle(0, 0, (size/tan(PI/3)), size, 2*(size/tan(PI/3)), 0);
+    moveDown();
+    resetMatrix();
+
+
+  }
+
+  public void moveDown() {
+    y+=speed;
+ 
+    
+    if (y-size > height) { 
+      y = 0;
+    }
+  }
+
 }
   public void settings() {  size(512, 512); }
   static public void main(String[] passedArgs) {
